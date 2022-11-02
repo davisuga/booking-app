c61a4e5c38fadeb74dbba66a63cf7b56d9e82faa
Davi Suga <daviciencia1@gmail.com>
2022-11-02 10:20:28 -0300

    fix: removed unused code

--- src/store/index.ts
+++ src/store/index.ts
@@ -8,11 +8,10 @@ export const store = configureStore({
   },
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+
 export type AppDispatch = typeof store.dispatch;
 
-export const useAppDispatch: () => AppDispatch = useDispatch; // Export a hook that can be reused to resolve types
+export const useAppDispatch: () => AppDispatch = useDispatch;
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
